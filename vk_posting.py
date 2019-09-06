999ce2b7f0403951b48232b0730661c79dafb001
Yuliya <sun51188@gmail.com>
2019-09-06 11:37:29 +0300

    Format tabs

--- vk_posting.py
+++ vk_posting.py
@@ -6,19 +6,19 @@ def post_to_vk(vk_token, album_id, group_id, image_path=None, message=''):
     vk = vk_session.get_api()
     upload = vk_api.VkUpload(vk_session)
     if image_path:
-    	photo = upload.photo(  
-        	image_path,
-        	album_id=album_id,
-        	group_id=group_id
-    	)
-    	vk_photo_url = f"https://vk.com/photo{photo[0]['owner_id']}_{photo[0]['id']}"
-    	return vk.wall.post(
-    		owner_id=f'-{group_id}', 
-    		message=message, 
-    		attachments=f"photo{photo[0]['owner_id']}_{photo[0]['id']}"
-    	)
+        photo = upload.photo(  
+            image_path,
+            album_id=album_id,
+            group_id=group_id
+        )
+        vk_photo_url = f"https://vk.com/photo{photo[0]['owner_id']}_{photo[0]['id']}"
+        return vk.wall.post(
+            owner_id=f'-{group_id}', 
+            message=message, 
+            attachments=f"photo{photo[0]['owner_id']}_{photo[0]['id']}"
+        )
     return vk.wall.post(
-    	owner_id=f'-{group_id}', 
-    	message=message, 
+        owner_id=f'-{group_id}', 
+        message=message, 
     )
     

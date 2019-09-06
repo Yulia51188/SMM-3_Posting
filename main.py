b5e5900b163effff7fa70d6f2a9e2818f51529ab
Yuliya <sun51188@gmail.com>
2019-09-06 14:31:21 +0300

    Debug vk and telegram posting

--- main.py
+++ main.py
@@ -9,31 +9,26 @@ def main():
     vk_token = os.getenv("ACCESS_TOKEN")
     group_id = os.getenv("GROUP_ID")
     album_id = os.getenv("ALBUM_ID")
-    vk_post_result = list(vk_posting.post_to_vk(
-        vk_token, 
-        album_id, 
-        group_id, 
-        'New message',
-        'rabbit.png',
+    print(vk_posting.post_to_vk(
+      vk_token, 
+      album_id, 
+      group_id, 
+      'rabbit.png', 
+      'New message'
     ))
-    if not any(vk_post_result):
-        print('Text and image are posted in VK')
+    bot_token = os.getenv('TELEGRAM_BOT_TOKEN')
+    chat_id = os.getenv("CHANNEL_ID")
+    telegram_post_result = list(telegram_posting.post_to_telegram(
+        bot_token, 
+        chat_id, 
+        'New message',
+        'rabbit.png'
+    ))
+    if not any(telegram_post_result):
+        print('Text and image are posted in Telegram')
     else:
-        for error_text in vk_post_result:
+        for error_text in telegram_post_result:
             if error_text: print(error_text)
-    # bot_token = os.getenv('TELEGRAM_BOT_TOKEN')
-    # chat_id = os.getenv("CHANNEL_ID")
-    # telegram_post_result = list(telegram_posting.post_to_telegram(
-    #     bot_token, 
-    #     chat_id, 
-    #     'New message',
-    #     'rabbit.png'
-    # ))
-    # if not any(telegram_post_result):
-    #     print('Text and image are posted in Telegram')
-    # else:
-    #     for error_text in telegram_post_result:
-    #         if error_text: print(error_text)
 
 
 if __name__=='__main__':

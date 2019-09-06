75e049ff48ac7cd5c99852d30f1e46311151118e
Yuliya <sun51188@gmail.com>
2019-09-06 11:37:01 +0300

    Add telegram_posting function

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 import vk_posting
+import telegram_posting
 
 
 def main():
@@ -8,9 +9,27 @@ def main():
     vk_token = os.getenv("ACCESS_TOKEN")
     group_id = os.getenv("GROUP_ID")
     album_id = os.getenv("ALBUM_ID")
-    print(album_id)
-    print(vk_posting.post_to_vk(vk_token, album_id, group_id, 'rabbit.png', 'New message'))
+    print(vk_posting.post_to_vk(
+      vk_token, 
+      album_id, 
+      group_id, 
+      'rabbit.png', 
+      'New message'
+    ))
+    bot_token = os.getenv('TELEGRAM_BOT_TOKEN')
+    chat_id = os.getenv("CHANNEL_ID")
+    telegram_post_result = list(telegram_posting.post_to_telegram(
+        bot_token, 
+        chat_id, 
+        'New message',
+        'rabbit.png'
+    ))
+    if not any(telegram_post_result):
+        print('Text and image are posted in Telegram')
+    else:
+        for error_text in telegram_post_result:
+            if error_text: print(error_text)
 
 
 if __name__=='__main__':
-	main()
+    main()
--- telegram_posting.py
+++ telegram_posting.py
@@ -1,9 +1,56 @@
 import telegram
+from telegram.error import InvalidToken, TimedOut, NetworkError
+import os
+from dotenv import load_dotenv
+from time import sleep
 
-def post_to_vk(bot_token, chat_id, image_path=None, message=''):
-	bot = telegram.Bot(token=bot_token)
-	response_text = bot.send_message(chat_id=chat_id, text=message)
-	yield response_text
-    if image_path:
-    	yield bot.send_photo(chat_id=chat_id, photo=open(image_path, 'rb'))
 
+def post_to_telegram(bot_token, chat_id, message='', image_path=None):
+    try:
+        bot = telegram.Bot(token=bot_token)
+    except InvalidToken as error:
+        return(f"Error occured, message can't be posted in Telegram: {error}") 
+    yield post_text_to_telegram(bot, 2394878, message) 
+    yield post_image_to_telegram(bot, chat_id, image_path)
+
+
+def post_text_to_telegram(bot, chat_id, message):
+    try:
+        bot.send_message(chat_id=chat_id, text=message)
+    except NetworkError as error:       
+        return(f"Error occured, text can't be posted in Telegram: {error}")  
+    except TimedOut as error:
+        sleep(30)
+        bot.send_message(chat_id=chat_id, text=message)
+
+
+def post_image_to_telegram(bot, chat_id, image_path):
+    if not os.path.isfile(image_path):
+        return("File doesn't exist")
+    try:
+        bot.send_photo(chat_id=chat_id, photo=open(image_path, 'rb'))
+    except NetworkError as error:
+        return(f"Error occured, image can't be posted in Telegram: {error}")  
+    except TimedOut as error:
+        sleep(30)
+        bot.send_photo(chat_id=chat_id, photo=open(image_path, 'rb'))
+
+
+def print_hello(message):
+    print(f'Hello, {message}')
+
+
+def main():
+    load_dotenv()
+    bot_token = os.getenv('TELEGRAM_BOT_TOKEN')
+    chat_id = os.getenv("CHANNEL_ID")
+    post_to_telegram(
+        bot_token, 
+        chat_id, 
+        'Good morning'
+        'rabbit.png' 
+    )
+
+
+if __name__=='__main__':
+    main()

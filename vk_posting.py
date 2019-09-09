32b472ddb348c5c8cc6e458def45e57c4d185ade
Yuliya <sun51188@gmail.com>
2019-09-09 14:04:21 +0300

    Change args position

--- vk_posting.py
+++ vk_posting.py
@@ -1,7 +1,7 @@
 import vk_api
 
 
-def post_to_vk(vk_token, album_id, group_id, image_path=None, message=''):
+def post_to_vk(vk_token, album_id, group_id, message='', image_path=None,):
     vk_session = vk_api.VkApi(token=vk_token)
     vk = vk_session.get_api()
     upload = vk_api.VkUpload(vk_session)

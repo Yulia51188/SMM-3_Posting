779aaf807ccb5cfed341f1e5cf358098609f8b16
Yuliya <sun51188@gmail.com>
2019-09-11 11:53:50 +0300

    Add FB posting code

--- fb_posting.py
+++ fb_posting.py
@@ -1,23 +1,55 @@
 import requests 
+from requests.exceptions import ConnectionError
+
 
 def post_to_fb(token, group_id, message='', image_path=None ):
-    # url_text_template = 'https://graph.facebook.com/{page_id}/feed'
-    url_photo_template = 'https://graph.facebook.com/{page_id}/photos' 
+    image_obj = get_image_object(image_path)
+    if not image_obj:
+        yield "Photo can't be uploaded in FB" 
+        try:
+            response = post_only_text_to_fb(token, group_id, message)
+            if not response.ok or 'id' not in response.json().keys():
+                yield f"Error occured, post can't be published in FB:\n{response.json()}"
+        except ConnectionError as error:
+            yield f"Network error, post can't be published in FB: {error}"
+    try:
+        response = post_photo_and_text_to_fb(token, group_id, message, image_obj)
+        if not response.ok or 'id' not in response.json().keys():
+            yield f"Error occured, post can't be published in FB:\n{response.json()}"
+    except ConnectionError as error:
+        yield f"Network error, post can't be published in FB: {error}"
+    
 
-    print(url_photo_template.format(page_id = fb_group_id))
+def get_image_object(image_path):
+    try:
+        image_object = open(image_path,'rb')
+        return image_object
+    except (OSError, IOError) as error:
+        return   
 
+
+def post_only_text_to_fb(token, group_id, message=''):
+    url_text_template = 'https://graph.facebook.com/{page_id}/feed'
     response = requests.post(
-        url = url_photo_template.format(page_id = group_id),
+        url_text_template.format(page_id = group_id),
+        data = {
+            'message': message,
+            'access_token': token
+        }  
+    )
+    return response
+
+
+def post_photo_and_text_to_fb(token, group_id, message, image_obj):
+    url_photo_template = 'https://graph.facebook.com/{page_id}/photos'
+    response = requests.post(
+    url_photo_template.format(page_id = group_id),
         files = {
-            'files': open(image_path,'rb')
+            'files': image_obj
         },
         data = {
             'caption': message,
             'access_token': token
         }
     )
-
-    response.raise_for_status()
-    print(response.json())
-
-
+    return response
